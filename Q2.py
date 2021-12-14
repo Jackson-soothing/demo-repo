409de42eced21c85d398ba5d3af7665f916f189c
soothingzsx <73217199+soothingzsx@users.noreply.github.com>
2021-12-15 01:18:39 +0800

    Add files via upload

---
+++ Q2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 31 13:19:46 2021
+
+@author: soothing
+"""
+
+def exist():
+    try:
+        v
+        print('variable is defined')
+    except:
+        print('variable is not defined')
